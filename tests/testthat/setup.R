d6b8fd62224d44240045203b705c9c933734e3bd
Ales Pour <ales.pour@bonitoo.io>
2021-06-10 18:19:11 +0200

    test: add test for multiple Flux tables in single CSV table

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -6,7 +6,7 @@ test.token <- 'DcvGNmM_fyYW0sqcSlVyllcR90MITaTKge19P3iDJvnPmCdF2vnwiL888bocS4bmI
 test.org <- 'bonitoo'
 test.client <- InfluxDBClient$new(url = test.url, token = test.token, org = test.org)
 
-.airSensors.time = c(
+.airSensors.time5 = c(
   as.nanotime(1623232361000000000),
   as.nanotime(1623232371000000000),
   as.nanotime(1623232381000000000),
@@ -16,7 +16,7 @@ test.client <- InfluxDBClient$new(url = test.url, token = test.token, org = test
 
 test.airSensors.data = list(
   data.frame(
-    `_time` = .airSensors.time,
+    `_time` = .airSensors.time5,
     `_value` = c(
       as.integer64(549),
       as.integer64(547),
@@ -31,7 +31,7 @@ test.airSensors.data = list(
     check.names = FALSE
   ),
   data.frame(
-    `_time` = .airSensors.time,
+    `_time` = .airSensors.time5,
     `_value` = c(FALSE, FALSE, TRUE, TRUE, FALSE),
     `_field` = replicate(5, 'grounded'),
     `_measurement` = replicate(5, 'airSensors'),
@@ -40,7 +40,7 @@ test.airSensors.data = list(
     check.names = FALSE
   ),
   data.frame(
-    `_time` = .airSensors.time,
+    `_time` = .airSensors.time5,
     `_value` = c(71.7844100, 71.7684399, 71.7819928, 71.7487767, 71.7335579),
     `_field` = replicate(5, 'temperature'),
     `_measurement` = replicate(5, 'airSensors'),
@@ -52,7 +52,7 @@ test.airSensors.data = list(
 
 test.airSensors.data.pivoted = list(
   data.frame(
-    `_time` = .airSensors.time,
+    `_time` = .airSensors.time5,
     `_measurement` = replicate(5, 'airSensors'),
     region = replicate(5, 'south'),
     sensor_id = replicate(5, 'TLM0101'),
@@ -65,6 +65,55 @@ test.airSensors.data.pivoted = list(
     ),
     grounded = c(FALSE, FALSE, TRUE, TRUE, FALSE),
     temperature = c(71.7844100, 71.7684399, 71.7819928, 71.7487767, 71.7335579),
+    check.names = FALSE
+  )
+)
+
+.airSensors.time3 = head(.airSensors.time5, 3)
+
+test.airSensors.data.multi = list(
+  data.frame(
+    `_time` = .airSensors.time3,
+    `_value` = c(
+      as.integer64(549),
+      as.integer64(547),
+      as.integer64(563)
+    ),
+    `_field` = replicate(3, 'altitude'),
+    `_measurement` = replicate(3, 'airSensors'),
+    region = replicate(3, 'south'),
+    sensor_id = replicate(3, 'TLM0101'),
+    check.names = FALSE
+  ),
+  data.frame(
+    `_time` = .airSensors.time3,
+    `_value` = c(
+      as.integer64(549),
+      as.integer64(538),
+      as.integer64(554)
+    ),
+    `_field` = replicate(3, 'altitude'),
+    `_measurement` = replicate(3, 'airSensors'),
+    region = replicate(3, 'south'),
+    sensor_id = replicate(3, 'TLM0102'),
+    check.names = FALSE
+  ),
+  data.frame(
+    `_time` = .airSensors.time3,
+    `_value` = c(71.7844100, 71.7684399, 71.7819928),
+    `_field` = replicate(3, 'temperature'),
+    `_measurement` = replicate(3, 'airSensors'),
+    region = replicate(3, 'south'),
+    sensor_id = replicate(3, 'TLM0101'),
+    check.names = FALSE
+  ),
+  data.frame(
+    `_time` = .airSensors.time3,
+    `_value` = c(71.9616430, 71.9217034, 71.9037406),
+    `_field` = replicate(3, 'temperature'),
+    `_measurement` = replicate(3, 'airSensors'),
+    region = replicate(3, 'south'),
+    sensor_id = replicate(3, 'TLM0102'),
     check.names = FALSE
   )
 )
