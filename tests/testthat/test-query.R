39b7d6ae6bb9ffafce228487ced2b9f7add21928
Ales Pour <ales.pour@bonitoo.io>
2021-06-10 18:19:11 +0200

    test: add test for multiple Flux tables in single CSV table

--- tests/testthat/test-query.R
+++ tests/testthat/test-query.R
@@ -1,6 +1,7 @@
 .client = test.client
 .data = test.airSensors.data
 .data.pivoted = test.airSensors.data.pivoted
+.data.multi = test.airSensors.data.multi
 
 with_mock_api({
   test_that("query", {
@@ -18,5 +19,11 @@ with_mock_api({
   test_that("query empty result", {
     response <- .client$query(text='from(bucket: \\"r-testing\\") |> range(start: -10y) |> filter(fn: (r) => r._measurement == \\"doesnotexist\\")')
     expect_null(response)
+  })
+
+  test_that("query multiple tables in one csv table", {
+    response <- .client$query(text='from(bucket: \\"r-testing\\") |> range(start: -10y) |> filter(fn: (r) => r._measurement == \\"airSensors\\" and (r.sensor_id == \\"TLM0101\\" or r.sensor_id == \\"TLM0102\\") and r._field != \\"grounded\\") |> limit(n: 3) |> drop(columns: [\\"_start\\", \\"_stop\\"])')
+    expected <- .data.multi
+    expect_equal(response, expected)
   })
 })
