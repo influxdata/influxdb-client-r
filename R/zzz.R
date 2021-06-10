34c07ca59ef0b51135d4eec4274d8d62ffe1eeb4
Ales Pour <ales.pour@bonitoo.io>
2021-06-10 10:41:46 +0200

    refactor: remove unnecessary code

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  # set handlers for Flux datatypes deserialization
-  methods::setAs("character", "rfc3339",
-                 function(from) as.POSIXct(from, tz="UTC", format="%Y-%m-%dT%H:%M:%OSZ"))
-  methods::setAs("character", "integer64",
-                 function(from) bit64::as.integer64(from))
+  # set max fractional digits possible for POSIXct
+  options(digits.secs = 6)
 }
