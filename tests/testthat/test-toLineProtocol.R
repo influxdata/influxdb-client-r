6f5435fc281b4a5d72573a0e9f8cb05a019dd72c
Ales Pour <ales.pour@bonitoo.io>
2021-06-11 13:45:04 +0200

    test: add more serialization cases

--- tests/testthat/test-toLineProtocol.R
+++ tests/testthat/test-toLineProtocol.R
@@ -3,11 +3,11 @@
 .data.pivoted = test.airSensors.data.pivoted
 .lp = list(
   c(
-  "w-airSensors,region=south,sensor_id=TLM0101 altitude=549 1623232361000000000",
-  "w-airSensors,region=south,sensor_id=TLM0101 altitude=547 1623232371000000000",
-  "w-airSensors,region=south,sensor_id=TLM0101 altitude=563 1623232381000000000",
-  "w-airSensors,region=south,sensor_id=TLM0101 altitude=560 1623232391000000000",
-  "w-airSensors,region=south,sensor_id=TLM0101 altitude=544 1623232401000000000"
+  "w-airSensors,region=south,sensor_id=TLM0101 altitude=549i 1623232361000000000",
+  "w-airSensors,region=south,sensor_id=TLM0101 altitude=547i 1623232371000000000",
+  "w-airSensors,region=south,sensor_id=TLM0101 altitude=563i 1623232381000000000",
+  "w-airSensors,region=south,sensor_id=TLM0101 altitude=560i 1623232391000000000",
+  "w-airSensors,region=south,sensor_id=TLM0101 altitude=544i 1623232401000000000"
   ),
   c(
   "w-airSensors,region=south,sensor_id=TLM0101 grounded=false 1623232361000000000",
@@ -26,11 +26,29 @@
 )
 .lp.pivoted = list(
   c(
-    "w-airSensors,region=south,sensor_id=TLM0101 altitude=549,grounded=false,temperature=71.78441 1623232361000000000",
-    "w-airSensors,region=south,sensor_id=TLM0101 altitude=547,grounded=false,temperature=71.7684399 1623232371000000000",
-    "w-airSensors,region=south,sensor_id=TLM0101 altitude=563,grounded=true,temperature=71.7819928 1623232381000000000",
-    "w-airSensors,region=south,sensor_id=TLM0101 altitude=560,grounded=true,temperature=71.7487767 1623232391000000000",
-    "w-airSensors,region=south,sensor_id=TLM0101 altitude=544,grounded=false,temperature=71.7335579 1623232401000000000"
+    "w-airSensors,region=south,sensor_id=TLM0101 altitude=549i,grounded=false,temperature=71.78441 1623232361000000000",
+    "w-airSensors,region=south,sensor_id=TLM0101 altitude=547i,grounded=false,temperature=71.7684399 1623232371000000000",
+    "w-airSensors,region=south,sensor_id=TLM0101 altitude=563i,grounded=true,temperature=71.7819928 1623232381000000000",
+    "w-airSensors,region=south,sensor_id=TLM0101 altitude=560i,grounded=true,temperature=71.7487767 1623232391000000000",
+    "w-airSensors,region=south,sensor_id=TLM0101 altitude=544i,grounded=false,temperature=71.7335579 1623232401000000000"
+  )
+)
+.lp.pivoted.notags = list(
+  c(
+    "w-airSensors altitude=549i,grounded=false,temperature=71.78441 1623232361000000000",
+    "w-airSensors altitude=547i,grounded=false,temperature=71.7684399 1623232371000000000",
+    "w-airSensors altitude=563i,grounded=true,temperature=71.7819928 1623232381000000000",
+    "w-airSensors altitude=560i,grounded=true,temperature=71.7487767 1623232391000000000",
+    "w-airSensors altitude=544i,grounded=false,temperature=71.7335579 1623232401000000000"
+  )
+)
+.lp.specialcharacters = list(
+  c(
+    'w-air\\ Sensors,region\\,us=south\\ east,sensor\\ id=TLM0101 altitude=549i,grounded="x \\"false\\"",temperature=71.78441 1623232361000000000',
+    'w-air\\ Sensors,region\\,us=south\\ east,sensor\\ id=TLM0101 altitude=547i,grounded="x \\"false\\"",temperature=71.7684399 1623232371000000000',
+    'w-air\\ Sensors,region\\,us=south\\ east,sensor\\ id=TLM0101 altitude=563i,grounded="x \\"true\\"",temperature=71.7819928 1623232381000000000',
+    'w-air\\ Sensors,region\\,us=south\\ east,sensor\\ id=TLM0101 altitude=560i,grounded="x \\"true\\"",temperature=71.7487767 1623232391000000000',
+    'w-air\\ Sensors,region\\,us=south\\ east,sensor\\ id=TLM0101 altitude=544i,grounded="x \\"false\\"",temperature=71.7335579 1623232401000000000'
   )
 )
 
@@ -52,12 +70,12 @@ test_that("toLineProtocol", {
 
 test_that("toLineProtocol / pivoted", {
   # rename some columns in order to test non-default parameters
-  # change measurement value to avoid overwriting source
+  # also change measurement value to avoid overwriting source
   data <- lapply(.data.pivoted,
                  function(t) {
+                   t['_measurement'] <- replicate(5, 'w-airSensors')
                    colnames(t)[which(names(t) == '_time')] <- 'time'
                    colnames(t)[which(names(t) == '_measurement')] <- 'name'
-                   t['name'] <- replicate(5, 'w-airSensors')
                    return(t)
                  })
   lp <- .client$toLineProtocol(data, precision = 'ns',
@@ -67,6 +85,95 @@ test_that("toLineProtocol / pivoted", {
                                timeCol = 'time')
   expected <- .lp.pivoted
   expect_equal(lp, expected)
+})
+
+test_that("toLineProtocol / no tags", {
+  data <- lapply(.data.pivoted,
+                 function(t) {
+                   t['_measurement'] <- replicate(5, 'w-airSensors')
+                   return(t)
+                 })
+  lp <- .client$toLineProtocol(data, precision = 'ns',
+                               measurementCol = '_measurement',
+                               tagCols = NULL,
+                               fieldCols = c("altitude", "grounded", "temperature"),
+                               timeCol = '_time')
+  expected <- .lp.pivoted.notags
+  expect_equal(lp, expected)
+})
+
+test_that("toLineProtocol / special characters", {
+  data <- lapply(.data.pivoted,
+                 function(t) {
+                   t['_measurement'] <- replicate(5, 'w-air Sensors')
+                   t['region'] <- replicate(5, 'south east')
+                   t['grounded'] <- lapply(t['grounded'], function(x) {
+                     sprintf("x \"%s\"", tolower(as.character(x)))
+                   })
+                   colnames(t)[which(names(t) == 'region')] <- 'region,us'
+                   colnames(t)[which(names(t) == 'sensor_id')] <- 'sensor id'
+                   return(t)
+                 })
+  lp <- .client$toLineProtocol(data, precision = 'ns',
+                               measurementCol = '_measurement',
+                               tagCols = c("region,us", "sensor id"),
+                               fieldCols = c("altitude", "grounded", "temperature"),
+                               timeCol = '_time')
+  expected <- .lp.specialcharacters
+  expect_equal(lp, expected)
+
+# actual HTTP payload:
+# w-air\ Sensors,region\,us=south\ east,sensor\ id=TLM0101 altitude=549i,grounded="x \"false\"",temperature=71.78441 1623232361000000000
+# ...
+
+})
+
+test_that("toLineProtocol / x is not data.frame", {
+  data <- "not a data.frame"
+  f <- function () {
+    .client$toLineProtocol(data, precision = 'ns',
+                           measurementCol = 'no-measurement',
+                           tagCols = c("region", "sensor_id"),
+                           fieldCols = c("altitude", "grounded", "temperature"),
+                           timeCol = '_time')
+  }
+  expect_error(f(), "'x' must be data.frame")
+})
+
+test_that("toLineProtocol / x is not list of data.frame", {
+  data <- list(data.frame(), "not a data.frame")
+  f <- function () {
+    .client$toLineProtocol(data, precision = 'ns',
+                           measurementCol = 'no-measurement',
+                           tagCols = c("region", "sensor_id"),
+                           fieldCols = c("altitude", "grounded", "temperature"),
+                           timeCol = '_time')
+  }
+  expect_error(f(), "'x' must be data.frame")
+})
+
+test_that("toLineProtocol / NULL precision", {
+  data <- .data.pivoted
+  f <- function () {
+    .client$toLineProtocol(data, precision = NULL,
+                           measurementCol = 'no-measurement',
+                           tagCols = c("region", "sensor_id"),
+                           fieldCols = c("altitude", "grounded", "temperature"),
+                           timeCol = '_time')
+  }
+  expect_error(f(), "'precision' cannot be NULL")
+})
+
+test_that("toLineProtocol / invalid precision", {
+  data <- .data.pivoted
+  f <- function () {
+    .client$toLineProtocol(data, precision = 'xs',
+                           measurementCol = 'no-measurement',
+                           tagCols = c("region", "sensor_id"),
+                           fieldCols = c("altitude", "grounded", "temperature"),
+                           timeCol = '_time')
+  }
+  expect_error(f())
 })
 
 test_that("toLineProtocol / not existing measurement column", {
@@ -103,6 +210,42 @@ test_that("toLineProtocol / not existing field columns", {
                            timeCol = '_time')
   }
   expect_error(f(), "field columns not found in data frame: no-1,no-2")
+})
+
+test_that("toLineProtocol / measurementCol is not single column", {
+  data <- .data.pivoted
+  f <- function () {
+    .client$toLineProtocol(data, precision = 'ns',
+                           measurementCol = c("a", "b"),
+                           tagCols = c("region", "sensor_id"),
+                           fieldCols = c("altitude", "grounded", "temperature"),
+                           timeCol = '_time')
+  }
+  expect_error(f(), "'measurementCol' must select single column")
+})
+
+test_that("toLineProtocol / fieldCols is not single column", {
+  data <- .data.pivoted
+  f <- function () {
+    .client$toLineProtocol(data, precision = 'ns',
+                           measurementCol = '_measureement',
+                           tagCols = c("region", "sensor_id"),
+                           fieldCols = NULL,
+                           timeCol = '_time')
+  }
+  expect_error(f(), "'fieldCols' parameter cannot be empty")
+})
+
+test_that("toLineProtocol / time column is not single column", {
+  data <- .data.pivoted
+  f <- function () {
+    .client$toLineProtocol(data, precision = 'ns',
+                           measurementCol = '_measurement',
+                           tagCols = c("region", "sensor_id"),
+                           fieldCols = c("altitude", "grounded", "temperature"),
+                           timeCol = NULL)
+  }
+  expect_error(f(), "'timeCol' parameter must select single column")
 })
 
 test_that("toLineProtocol / not existing time column", {
