e37d17d336556756dbd125be56d8f6bd3eea56bb
Ales Pour <ales.pour@bonitoo.io>
2021-06-29 15:41:15 +0200

    test: add dep library

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,3 +1,7 @@
+# 3.x + testthat hack
+library(bit64)
+library(nanotime)
+
 # test helper function (https://www.r-bloggers.com/2020/10/capture-message-warnings-and-errors-from-a-r-function/)
 expect_anything = function(f) {
   messages <- warnings <- errors <- NULL
